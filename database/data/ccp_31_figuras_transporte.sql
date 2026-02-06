6ec45a5b0e8a3f86ecf9c6886e260cb4b7fb5955
akromicc <dannyjp49@gmail.com>
2026-02-05 20:02:13 -0400

    data

---
+++ database/data/ccp_31_figuras_transporte.sql
@@ -0,0 +1,8 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+INSERT INTO ccp_31_figuras_transporte VALUES('01','Operador','2024-07-17','');
+INSERT INTO ccp_31_figuras_transporte VALUES('02','Propietario','2024-07-17','');
+INSERT INTO ccp_31_figuras_transporte VALUES('03','Arrendador','2024-07-17','');
+INSERT INTO ccp_31_figuras_transporte VALUES('04','Notificado','2024-07-17','');
+INSERT INTO ccp_31_figuras_transporte VALUES('05','Integrante de Coordinados','2024-07-26','');
+COMMIT;
